2c174c6afa713df04fb9eaac479d6448311d43a6
Marcelo <Marcelo@Marcelo-PC>
2017-06-04 22:44:09 -0300

    Commoit

--- src/org/macunaima/gui/ui/EntityTabPanel.java
+++ src/org/macunaima/gui/ui/EntityTabPanel.java
@@ -2,6 +2,7 @@ package org.macunaima.gui.ui;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
+import java.util.Vector;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -11,8 +12,10 @@ import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.EtchedBorder;
+import javax.swing.table.DefaultTableModel;
 
 import org.macunaima.application.EmpresasApplication.EmpresasDisplay;
+import org.macunaima.domain.Empresa;
 
 public class EntityTabPanel extends JPanel implements EmpresasDisplay {
 
@@ -20,27 +23,20 @@ public class EntityTabPanel extends JPanel implements EmpresasDisplay {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private JButton searchButton;
+	private JTextField searchText;
+	private Vector<Empresa> data;
+	private JTable table;
 
 	public EntityTabPanel(String name, String newEmpresaName, String newEmpresaIconPath) {
 		// headers for the table
-		String[] columns = new String[] { "Id", "Name", "Hourly Rate", "Part Time" };
+		Vector<String> columns = new Vector<String>();
+		columns.add("Nome da Empresa");
+		columns.add("Desconto a Crédito");
+		columns.add("Desconto à vista");
 
 		// actual data for the table in a 2d array
-		Object[][] data = new Object[][] { { 1, "John", 40.0, false }, { 2, "Rambo", 70.0, false },
-				{ 3, "Zorro", 60.0, true }, { 3, "Zorro", 60.0, true }, { 3, "Zorro", 60.0, true },
-				{ 3, "Zorro", 60.0, true }, { 3, "Zorro", 60.0, true }, { 3, "Zorro", 60.0, true },
-				{ 3, "Zorro", 60.0, true }, { 3, "Zorro", 60.0, true }, { 3, "Zorro", 60.0, true },
-				{ 3, "Zorro", 60.0, true }, { 3, "Zorro", 60.0, true }, { 3, "Zorro", 60.0, true },
-				{ 3, "Zorro", 60.0, true }, { 3, "Zorro", 60.0, true }, { 3, "Zorro", 60.0, true },
-				{ 3, "Zorro", 60.0, true }, { 3, "Zorro", 60.0, true }, { 3, "Zorro", 60.0, true },
-				{ 3, "Zorro", 60.0, true }, { 3, "Zorro", 60.0, true }, { 3, "Zorro", 60.0, true },
-				{ 3, "Zorro", 60.0, true }, { 3, "Zorro", 60.0, true }, { 3, "Zorro", 60.0, true },
-				{ 3, "Zorro", 60.0, true }, { 3, "Zorro", 60.0, true }, { 3, "Zorro", 60.0, true },
-				{ 3, "Zorro", 60.0, true }, { 3, "Zorro", 60.0, true }, { 3, "Zorro", 60.0, true },
-				{ 3, "Zorro", 60.0, true }, { 3, "Zorro", 60.0, true }, { 3, "Zorro", 60.0, true },
-				{ 42, "Zorro", 60.0, true }, { 3, "Zorro", 60.0, true }, { 3, "Zorro", 60.0, true },
-				{ 40, "Zorro", 60.0, true }, };
-
+		data = new Vector<Empresa>();
 		setLayout(new BorderLayout());
 		JPanel panel_7 = new JPanel();
 		add(panel_7, BorderLayout.CENTER);
@@ -51,16 +47,15 @@ public class EntityTabPanel extends JPanel implements EmpresasDisplay {
 		panel_8.setBorder(new EtchedBorder(EtchedBorder.RAISED, null, null));
 		panel_7.add(panel_8);
 
-		JTextField textField = new JTextField();
-		textField.setToolTipText("Pesquisar");
-		panel_8.add(textField);
-		textField.setColumns(30);
+		searchText = new JTextField();
+		searchText.setToolTipText("Pesquisar");
+		panel_8.add(searchText);
+		searchText.setColumns(30);
 
-		JButton btnNewButton_4 = new JButton("");
-		btnNewButton_4.setToolTipText("Pesquisar");
-		btnNewButton_4
-				.setIcon(new ImageIcon("C:\\Users\\Marcelo\\workspace\\macunaima-admin\\img\\search-loop-12.png"));
-		panel_8.add(btnNewButton_4);
+		searchButton = new JButton("");
+		searchButton.setToolTipText("Pesquisar");
+		searchButton.setIcon(new ImageIcon("C:\\Users\\Marcelo\\workspace\\macunaima-admin\\img\\search-loop-12.png"));
+		panel_8.add(searchButton);
 
 		JButton btnNewButton_3 = new JButton("");
 		btnNewButton_3.setToolTipText(newEmpresaName);
@@ -71,7 +66,7 @@ public class EntityTabPanel extends JPanel implements EmpresasDisplay {
 		add(panel_9, BorderLayout.SOUTH);
 		panel_9.setBorder(new EmptyBorder(0, 0, 0, 0));
 		// create table with data
-		JTable table = new JTable(data, columns);
+		table = new JTable(data, columns);
 		JScrollPane scrollPane = new JScrollPane(table);
 		panel_9.add(scrollPane);
 
@@ -80,6 +75,31 @@ public class EntityTabPanel extends JPanel implements EmpresasDisplay {
 	@Override
 	public Component getContent() {
 		return this;
+	}
+
+	@Override
+	public JButton getSearchButton() {
+		return searchButton;
+	}
+
+	@Override
+	public String getSearchText() {
+		return searchText.getText();
+	}
+
+	@Override
+	public void put(Vector<Empresa> empresas) {
+		DefaultTableModel tableModel = (DefaultTableModel) table.getModel();
+		tableModel.setRowCount(0);
+		for (int i = 0; i < empresas.size(); i++) {
+			String[] data = new String[3];
+			data[0] = empresas.get(i).getNome();
+			data[1] = empresas.get(i).getDescontoCredito();
+			data[2] = empresas.get(i).getDescontoAVista();
+			tableModel.addRow(data);
+		}
+		table.setModel(tableModel);
+		tableModel.fireTableDataChanged();
 	}
 
 }
